22be0633892d419a9b3cfb6138970ca82e56b9b6
Keith Oppel <koppel@Keiths-MacBook-Pro.local>
2017-01-18 09:17:44 -0800

    add functionality to quieres file: updateName, deleteTask

--- database/queries.js
+++ database/queries.js
@@ -3,13 +3,21 @@ const db = pgp({ database: 'doitnow' })
 
 const allTasks = 'SELECT * from todos'
 const createTask = 'INSERT INTO todos (id, name) VALUES ($1, $2) RETURNING *'
+const updateName = 'UPDATE todos SET name=$1 WHERE ID=$2 RETURNING *'
+const deleteTask = 'DELETE from todos WHERE id =$1'
 
 
 const Tasks = {
   getAll: () => {
     return db.any( allTasks )
   },
-  create: (id, name) => {
+  create: ( id, name ) => {
     return db.one ( createTask, [id, name] )
   },
+  update: ( id, name ) => {
+    return db.one ( updateName [id, name] )
+  },
+  deleteTask: ( id ) => {
+    return db.none ( deleteTask [id] )
+  }  
 }
