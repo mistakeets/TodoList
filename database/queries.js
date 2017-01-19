cb3a8c222209993824783c89c5673bba68ad569b
Debrena McEwen <debrenamcewen@gmail.com>
2017-01-18 19:08:01 -0800

    add create functionality to application

--- database/queries.js
+++ database/queries.js
@@ -1,14 +1,11 @@
-const pgp = require( 'pg-promise' )()
-const db = pgp({ database: 'doitnow' })
+const Queries = {
+  allTasks: 'SELECT * from todos',
 
-const allTasks = 'SELECT * from todos'
-const createTask = 'INSERT INTO todos (id, name) VALUES ($1, $2) RETURNING *'
-const updateName = 'UPDATE todos SET name=$1 WHERE ID=$2 RETURNING *'
-const deleteTask = 'DELETE from todos WHERE id =$1'
+  createTask: 'INSERT INTO todos (name) VALUES ($1) RETURNING *',
 
-module.exports = {
-  allTasks,
-  createTask,
-  updateName,
-  deleteTask
+  updateName: 'UPDATE todos SET name=$1 WHERE ID=$2 RETURNING *',
+
+  deleteTask: 'DELETE from todos WHERE id =$1'
 }
+
+module.exports = Queries;
